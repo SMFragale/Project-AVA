7d07b1a87c3f64ef2f5b2866a99b775e296f949b
sebastianMF <smolanof@gmail.com>
2023-06-03 22:46:19 -0500

    Fixes: Deletes bullets after some time. Features: Added a camera motor to follow the player around

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
+
+    // Maybe add an explosion effect here
     private void OnCollisionEnter(Collision other) {
         if(other.gameObject.CompareTag("Shootable")) {
             Destroy(gameObject);
--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -15,8 +15,10 @@ public class Shooter : MonoBehaviour
     private float projectileSpeed = 10f;
     
     public void Shoot(Vector3 direction) {
+        //In the future this should be done within a pool
         var projectile = Instantiate(projectilePrefab, origin.position, Quaternion.identity);
         AudioSource.PlayClipAtPoint(shootSound, origin.position);
+        Destroy(projectile, 5f);
         projectile.GetComponent<Rigidbody>().velocity = direction * projectileSpeed;
     }
 }

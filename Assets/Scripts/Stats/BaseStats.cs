bf763ad760336179d26179466e10d3bfdab0030b
sebastianMF <62181279+SMFragale@users.noreply.github.com>
2023-08-18 23:13:04 -0400

    Added small fixes and documentation to the Stats namespace

--- Assets/Scripts/Stats/BaseStats.cs
+++ Assets/Scripts/Stats/BaseStats.cs
@@ -3,10 +3,27 @@ using UnityEngine;
 
 namespace AVA.Stats
 {
+    /// <summary>
+    /// BaseStats represents the runtime stats model.
+    /// It holds a dictionary of stats and their values. These BaseStats are a runtime representation of the <see cref="BaseStatsSO"/>.
+    /// Despite being a runtime representation, these stats are immutable, the only way to modify stats in-game is to use the <see cref="CharacterModifiers"/> class.
+    /// </summary>
+
     public class BaseStats
     {
-        public Dictionary<StatType, float> stats { get; private set; } = new Dictionary<StatType, float>();
+        private Dictionary<StatType, float> stats = new Dictionary<StatType, float>();
 
+        /// <summary>
+        /// Returns a copy of the stats dictionary.
+        /// </summary>
+        /// <typeparam name="StatType">The type of a stat</typeparam>
+        /// <typeparam name="float">The value of a stat</typeparam>
+        public Dictionary<StatType, float> Stats { get { return new Dictionary<StatType, float>(stats); } }
+
+        /// <summary>
+        /// Creates a new BaseStats object from a <see cref="BaseStatsSO"/>. It will log an error if the BaseStatsSO is null.
+        /// </summary>
+        /// <param name="so">A base stats Scriptable Object that contains the </param>
         public BaseStats(BaseStatsSO so)
         {
             if (so == null)
@@ -21,29 +38,27 @@ namespace AVA.Stats
             }
         }
 
+        /// <param name="type">The type of the desired stat</param>
+        /// <returns>The value of a stat.</returns>
+        /// <exception cref="NoSuchStatException">If the stat does not exist, it will throw a <see cref="NoSuchStatException"/>.</exception>
         public float GetStat(StatType type)
         {
-            float value;
-            if (stats.TryGetValue(type, out value))
+            if (stats.TryGetValue(type, out float value))
                 return value;
             else
                 throw new NoSuchStatException(type);
         }
 
-        public void SetStat(StatType type, float newStat)
-        {
-            if (stats.ContainsKey(type))
-            {
-                stats[type] = newStat;
-            }
-        }
-
+        /// <returns>A list of all the stat types in this object. It's a good idea to check if the base stats contains a stat before trying to get it.</returns>
         public List<StatType> GetStatTypes()
         {
             return new List<StatType>(stats.Keys);
         }
     }
 
+    /// <summary>
+    /// This exception is thrown when trying to get a stat that does not exist in the <see cref="BaseStats"/> object.
+    /// </summary>
     public class NoSuchStatException : System.Exception
     {
         public NoSuchStatException(StatType type) : base("The stat " + type + " does not exist in this object") { }
--- Assets/Scripts/Stats/MonoBehaviors/CharacterStats.cs
+++ Assets/Scripts/Stats/MonoBehaviors/CharacterStats.cs
@@ -3,47 +3,80 @@ using UnityEngine.Events;
 
 namespace AVA.Stats
 {
+
+    /// <summary>
+    /// This component is responsible for exposing methods related to the stats of the game. However it only contains methods to read from stats and to subscribe to stat events. 
+    /// For a component that can modify stats, see <see cref="CharacterModifiers"/>.
+    /// To add this component, the gameobject must have a <see cref="StatsController"/> component as well.
+    /// </summary>
     public class CharacterStats : StatOutput
     {
+
+        /// <param name="type"> The stat type</param>
+        /// <returns> The value of the stat associated with <paramref name="type"> </returns>
         public float GetStat(StatType type)
         {
             return statServiceInstance.GetCalculatedStat(type);
         }
 
+        /// <param name="type"> The stat type</param>
+        /// <returns> The base value of the stat associated with <paramref name="type"> </returns>
         public float GetBaseStat(StatType type)
         {
             return statServiceInstance.GetBaseStat(type);
         }
 
+        /// <returns> A list of all the stat types associated with the relevant <see cref="StatsController"> </returns>
         public List<StatType> GetStatTypes()
         {
             return statServiceInstance.GetStatTypes();
         }
 
+        /// <returns> A dictionary of all the calculated stat types and their respective values. </returns>
         public Dictionary<StatType, float> GetAllCalculatedStats()
         {
             return statServiceInstance.GetAllCalculatedStats();
         }
 
-        public Dictionary<StatType, float> GetAllBaseStats() {
+        /// <returns> A dictionary of all the base stats and their respective values. </returns>
+        public Dictionary<StatType, float> GetAllBaseStats()
+        {
             return statServiceInstance.GetAllBaseStats();
         }
 
+        /// <summary>
+        /// Adds a listener to an event that is called when the stat associated with <paramref name="type"> changes.
+        /// </summary>
+        /// <param name="type"> Type of the stat to add a listener to</param>
+        /// <param name="listener"> Action to be executed when the specific stat changes</param>
         public void AddStatListener(StatType type, UnityAction listener)
         {
             statServiceInstance.AddStatListener(type, listener);
         }
 
+        /// <summary>
+        /// Removes a listener from an event that is called when a stat associated with <paramref name="type"> changes.
+        /// </summary>
+        /// <param name="type"> Type of the stat to remove a listener from</param>
+        /// <param name="listener"> Action that will be removed from the event</param>
         public void RemoveStatListener(StatType type, UnityAction listener)
         {
             statServiceInstance.RemoveStatListener(type, listener);
         }
 
+        /// <summary>
+        /// Adds a listener to an event that is called when any stat changes.
+        /// </summary>
+        /// <param name="listener"> Action that will be executed when any stat changes</param>
         public void AddOnStatsChangedListener(UnityAction listener)
         {
             statServiceInstance.AddOnStatsChangedListener(listener);
         }
 
+        /// <summary>
+        /// Removes a listener from an event that is called when any stat changes.
+        /// </summary>
+        /// <param name="listener"> Action that will be removed from the event</param>
         public void RemoveOnStatsChangedListener(UnityAction listener)
         {
             statServiceInstance.RemoveOnStatsChangedListener(listener);
